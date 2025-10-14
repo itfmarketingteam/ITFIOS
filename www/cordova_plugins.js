d8902ae7d9f7abde2e7ddbb610010823fc23337c
Karen Petrosyan <karen@vyyer.com>
2025-10-14 10:11:50 +0400

    14.10.2025 harut update

---
+++ www/cordova_plugins.js
@@ -0,0 +1,33 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-inappbrowser.inappbrowser",
+      "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+      "pluginId": "cordova-plugin-inappbrowser",
+      "clobbers": [
+        "cordova.InAppBrowser.open"
+      ]
+    },
+    {
+      "id": "cordova-plugin-ionic-webview.IonicWebView",
+      "file": "plugins/cordova-plugin-ionic-webview/src/www/util.js",
+      "pluginId": "cordova-plugin-ionic-webview",
+      "clobbers": [
+        "Ionic.WebView"
+      ]
+    },
+    {
+      "id": "cordova-plugin-ionic-webview.ios-wkwebview-exec",
+      "file": "plugins/cordova-plugin-ionic-webview/src/www/ios/ios-wkwebview-exec.js",
+      "pluginId": "cordova-plugin-ionic-webview",
+      "clobbers": [
+        "cordova.exec"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-inappbrowser": "6.0.0",
+    "cordova-plugin-ionic-webview": "5.0.0",
+    "cordova-plugin-whitelist": "1.3.5"
+  };
+});
