edbe0f6c3e05999825f4daef758f1df03ef5783a
Karen Petrosyan <karen@vyyer.com>
2025-10-29 11:33:53 +0400

    harut 29.10.25

--- www/cordova_plugins.js
+++ www/cordova_plugins.js
@@ -28,6 +28,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports.metadata = {
     "cordova-plugin-inappbrowser": "6.0.0",
     "cordova-plugin-ionic-webview": "5.0.0",
-    "cordova-plugin-whitelist": "1.3.5"
+    "cordova-plugin-whitelist": "1.3.5",
+    "cordova-plugin-splashscreen": "6.0.1"
   };
 });
